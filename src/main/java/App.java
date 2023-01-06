f9a5c96600225252057587a27d9d15b9f2eeec46
RomanKomissarov <komissarov369@gmail.com>
2023-01-06 22:55:07 +0300

    Task has ready

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,11 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld helloWorld = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat firstCat = (Cat) applicationContext.getBean("cat");
+        Cat secondCat = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean == helloWorld);
+        System.out.println(firstCat == secondCat);
     }
 }
